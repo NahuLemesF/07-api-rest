cc7613b4ae2aa51395b8d6879c61621879c4ad91
Nahuel Lemes <nahulem@gmail.com>
2025-01-04 14:53:45 -0300

    feat(models): crear entidades iniciales.

---
+++ src/main/java/com/example/restaurant_management/models/Client.java
@@ -0,0 +1,41 @@
+package com.example.restaurant_management.models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Email(message = "Email no válido")
+    private String email;
+    private Boolean isFrequent = false;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders = new ArrayList<>();
+
+    public Client(Long id, String name, String email, Boolean isFrequent) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.isFrequent = isFrequent;
+    }
+
+    public Client() {
+    }
+}
---
+++ src/main/java/com/example/restaurant_management/models/Order.java
@@ -0,0 +1,48 @@
+package com.example.restaurant_management.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime orderDate;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_dishes",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "dish_id"))
+    private List<Dish> dishes;
+    private Float totalPrice;
+
+    public Order(Client client, List<Dish> dishes, Long id, LocalDateTime orderDate, Float totalPrice) {
+        this.client = client;
+        this.dishes = dishes;
+        this.id = id;
+        this.orderDate = orderDate;
+        this.totalPrice = totalPrice;
+    }
+
+    public Order() {
+    }
+}
