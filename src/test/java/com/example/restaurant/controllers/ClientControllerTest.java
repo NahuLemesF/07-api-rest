a24cbce9010bbe2b4416ca44e065ae0c42066ed3
Nahuel Lemes <nahulem@gmail.com>
2025-01-08 13:26:28 -0300

    test(project): inicializar tests

---
+++ src/test/java/com/example/restaurant/controllers/ClientControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.restaurant.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addClient() {
+    }
+
+    @Test
+    void getClientById() {
+    }
+
+    @Test
+    void getAllClients() {
+    }
+
+    @Test
+    void updateClient() {
+    }
+
+    @Test
+    void deleteClient() {
+    }
+}
